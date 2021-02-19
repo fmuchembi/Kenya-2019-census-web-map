02498dc5f5cec3801a8ea86f0e128c363f97e07d
faith <fmuchembi@gmail.com>
2021-02-19 13:02:52 +0300

    display county boundaries

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Map from './components/Map';
 import Key from './components/Key';
  
 
 const App = () => {
-   const [counties, setCounties] = useState(['hello'])
-    
    return(
         <div className='html'>
-         {counties.Length === 0?(<h1>Loading</h1>):
-         ( <div>
-             <Map/>
-             <Key/>
-           </div>
-         )} 
+            <Map/>
+            <Key/> 
         </div>
     );   
 };
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup, GeoJSON} from 'react-leaflet';
-import {features} from './data/data.json';
+import {features} from './data/counties.json';
+import Key from './Key';
 
 const Map = () => {
     const style = {
         height: '90vh',
         width: '100%',
     }
+    const countyStyle={
+        fillColor: '#FF4500',
+        weight: 0.5,
+        opacity: 4,
+        color: 'black',
+        dashArray: '3',
+        fillOpacity: 0.5
+
+    }
+    console.log(features)
+    
 
     return (
         <div className='html'>
@@ -14,10 +26,16 @@ const Map = () => {
             <div className="row no-gutters">
                 <div className="col-sm-6 col-md-8">
                     <MapContainer center={[1.286389, 38.817223]}
-                        zoom={7} scrollWheelZoom={false} style={style}>
+                        zoom={6} scrollWheelZoom={false} style={style}>
                         <TileLayer
-                            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                            attribution="Map tiles by Carto, under CC BY 3.0. Data by OpenStreetMap, under ODbL."
+                            url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png"
+                        />
+                        <GeoJSON data={features} style={countyStyle} />
+                        <Marker position={[1.2578, 37.5]}>
+                            <Popup>I am a green leaf</Popup>
+                        </Marker>
+                        <h1>Helo world</h1>
                     </MapContainer>
                 </div>
             </div>
